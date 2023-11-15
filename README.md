b4a051bfe3fb4ba2463c315bb1532c9bd42bba39
jgarcia <juanfagner@gmail.com>
2023-11-15 14:22:01 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Calculadora
+
+Criação de calculadora simples e classe de testes.
+
+	- Teste de Soma
+	- Teste de Média
+	- Teste de Binario
+	- Teste de Número de Dígitos
+	- Teste de Número Primo
+	- Teste de Palíndromo
+
+## Instalação
+
+Utilize o [pip](https://pip.pypa.io/en/stable/) para instalar Calculadora.py
+
+pip install package_name
+
+
+## Como usar
+```python
+from Calculadora import TesteCalculadora
+TesteCalculadora()
+```
+
+## Author
+Juan Garcia
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="Calculadora",
+    version="0.0.1",
+    author="Juan Garcia",
+    author_email="juanfagner@gmail.com",
+    description="Leitura de PDF",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8'
+)
